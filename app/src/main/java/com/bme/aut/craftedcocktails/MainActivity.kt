2300330e5e41bf7dc0ef1a95fa5a8af8b808a826
Urbán Norbert <urban.norbert.bme@gmail.com>
2021-04-01 15:58:18 +0200

    Repository initialization

---
+++ app/src/main/java/com/bme/aut/craftedcocktails/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bme.aut.craftedcocktails
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
