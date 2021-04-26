00c9371ff5c4a84352820bc6afd3d30bcdd5b512
Urbán Norbert <urban.norbert.bme@gmail.com>
2021-04-26 13:13:06 +0200

    add recycler view and adapter to main screen

--- app/src/main/java/com/bme/aut/craftedcocktails/ui/main/MainScreenFragment.kt
+++ app/src/main/java/com/bme/aut/craftedcocktails/ui/main/MainScreenFragment.kt
@@ -2,13 +2,20 @@ package com.bme.aut.craftedcocktails.ui.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DividerItemDecoration
 import co.zsmb.rainbowcake.base.OneShotEvent
 import co.zsmb.rainbowcake.base.RainbowCakeFragment
 import co.zsmb.rainbowcake.dagger.getViewModelFromFactory
 import co.zsmb.rainbowcake.extensions.exhaustive
 import com.bme.aut.craftedcocktails.R
+import com.bme.aut.craftedcocktails.model.Cocktail
+import com.bme.aut.craftedcocktails.ui.main.adapter.CocktailsAdapter
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainScreenFragment : RainbowCakeFragment<MainScreenViewState, MainScreenViewModel>() {
+
+    private lateinit var cocktailsAdapter: CocktailsAdapter
 
     override fun provideViewModel() = getViewModelFromFactory()
 
@@ -16,6 +23,17 @@ class MainScreenFragment : RainbowCakeFragment<MainScreenViewState, MainScreenVi
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        cocktailsAdapter =
+            CocktailsAdapter(requireContext(), ::onCocktailItemClicked, ::onCocktailItemLongClicked)
+        recycler_view.adapter = cocktailsAdapter
+        recycler_view.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+
         viewModel.loadCocktails()
     }
 
@@ -24,8 +42,13 @@ class MainScreenFragment : RainbowCakeFragment<MainScreenViewState, MainScreenVi
             Initial -> {
             }
             Loading -> {
+                progress_bar.isVisible = true
+                recycler_view.isVisible = false
             }
             is DataReady -> {
+                cocktailsAdapter.submitList(viewState.result)
+                progress_bar.isVisible = false
+                recycler_view.isVisible = true
             }
             NetworkError -> {
             }
@@ -33,5 +56,13 @@ class MainScreenFragment : RainbowCakeFragment<MainScreenViewState, MainScreenVi
     }
 
     override fun onEvent(event: OneShotEvent) {
+    }
+
+    private fun onCocktailItemClicked(cocktail: Cocktail) {
+
+    }
+
+    private fun onCocktailItemLongClicked(cocktail: Cocktail): Boolean {
+        return true
     }
 }
